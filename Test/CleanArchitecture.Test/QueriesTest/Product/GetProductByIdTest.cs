47f03ae387ce0eac6b7ff5358a2dfca5803539a8
nghia46 <nghialoe46@gmail.com>
2024-04-30 16:56:54 +0700

    add getProductByIdQuery Test

--- Test/CleanArchitecture.Test/QueriesTest/Product/GetProductByIdTest.cs
+++ Test/CleanArchitecture.Test/QueriesTest/Product/GetProductByIdTest.cs
@@ -1,17 +1,17 @@
-﻿using CleanArchitecture.Application.Queries;
+
+using CleanArchitecture.Application.Queries;
 using CleanArchitecture.Domain.Entities;
 using CleanArchitecture.Domain.Interfaces;
 using Newtonsoft.Json;
 using Telerik.JustMock;
 
 namespace CleanArchitecture.Test.QueriesTest;
-
 [TestClass]
-public class GetProductsTest
+public class GetProductByIdTest
 {
     public TestContext? TestContext { get; set; }
     [TestMethod]
-    public async Task GivenGetProductsQueryHandler_WhenHandleCalled_ThenReturnProducts()
+    public async Task GivenGetProductByIdQueryHandler_WhenHandleCalled_ThenReturnProduct()
     {
         // Arrange
         var products = new List<Product>
@@ -20,34 +20,37 @@ public class GetProductsTest
             {
                 Id = Guid.NewGuid(),
                 Name = "Product 1",
-                Price = 21.16M
+                Price = 100
             },
             new()
             {
                 Id = Guid.NewGuid(),
                 Name = "Product 2",
-                Price = 10.32M
+                Price = 200
+            },
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Product 3",
+                Price = 300
             }
         };
 
         var mockRepository = Mock.Create<IProductRepository>();
-        Mock.Arrange(() => mockRepository.GetProductsAsync())
-            .Returns(Task.FromResult<IEnumerable<Product>>(products));
+        Mock.Arrange(() => mockRepository.GetProductByIdAsync(Arg.IsAny<Guid>()))
+           .Returns(Task.FromResult<Product>(products[0]));
 
-        var handler = new GetProductsQueryHandler(mockRepository);
-        var query = new GetProductsQuery();
-
+        var handler = new GetProductByIdQueryHandler(mockRepository);
+        var query = new GetProductByIdQuery(products[0].Id);
+        
         // Act
         var result = await handler.Handle(query, CancellationToken.None);
-
         // Assert
         Assert.IsNotNull(result, "Result should not be null");
-        var expectedProductNames = new List<string> { "Product 1", "Product 2" };
-        foreach (var product in result)
-        {
-            Assert.IsTrue(expectedProductNames.Contains(product.Name), $"Product name '{product.Name}' should be in the expected list");
-        }
-        Assert.AreEqual(2, result.Count());
+        Assert.AreEqual(products[0].Id, result.Id);
+        Assert.AreEqual(products[0].Name, result.Name);
+        Assert.AreEqual(products[0].Price, result.Price);
+
         var resultJson = JsonConvert.SerializeObject(result);
         TestContext?.WriteLine(resultJson);
     }
